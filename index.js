4d75236f1b186a5e5ac0170727ba2e5c0a9a2d7a
Joel Gecale <jgecale_5@yahoo.com>
2024-11-01 17:26:33 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+import express from "express";
+import axios from "axios";
+import bodyParser from "body-parser";
+import { truncateSync } from "fs";
+
+
+const app = express();
+const port = 3000;
+const API_URL = "https://api.lyrics.ovh/v1/";
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", (req, res) => {
+   res.render("index.ejs");
+});
+
+app.post("/submit", async (req, res) => {
+    try {
+       const result = await axios.get(API_URL + req.body.artist + "/" + req.body.title);
+       res.render("index.ejs", {lyrics: req.body.title + "\n" + req.body.artist + "\n\n" + result.data.lyrics});
+    } catch (error){
+        console.log(error);
+        res.render("index.ejs", {lyrics: "Unable to search the lyrics based on this criteria."});
+        res.status(500);
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
